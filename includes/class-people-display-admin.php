e08539657ed378c97d6c4de19e56baf607a26443
Jeremy Felt <jeremy.felt@gmail.com>
2017-01-17 11:07:38 -0800

    Echo instead of return errors when displaying profile

--- includes/class-people-display-admin.php
+++ includes/class-people-display-admin.php
@@ -111,19 +111,19 @@ class WSUWP_People_Display_Admin {
 		$response = wp_remote_get( $request_url );
 
 		if ( is_wp_error( $response ) ) {
-			return '<!-- ' . sanitize_text_field( $response->get_error_message() ) . ' -->';
+			echo '<!-- ' . sanitize_text_field( $response->get_error_message() ) . ' -->';
 		}
 
 		$data = wp_remote_retrieve_body( $response );
 
 		if ( empty( $data ) ) {
-			return '<!-- empty -->';
+			echo '<!-- empty -->';
 		}
 
 		$person = json_decode( $data );
 
 		if ( empty( $person ) ) {
-			return '<!-- empty -->';
+			echo '<!-- empty -->';
 		}
 
 		$name = $person->title->rendered;
